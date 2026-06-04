63c67d0145dc94a9c200aca970f98a157e47e1d3
ThBTT! <thiagotelles.contato@gmail.com>
2026-06-04 14:27:26 -0300

    Port to Minecraft 1.21.4

--- src/main/java/net/thbtt/favoritelanguage/mixin/LanguageSelectionListWidgetMixin.java
+++ src/main/java/net/thbtt/favoritelanguage/mixin/LanguageSelectionListWidgetMixin.java
@@ -1,23 +1,21 @@
 package net.thbtt.favoritelanguage.mixin;
 
+import net.minecraft.client.gui.widget.ContainerWidget;
 import net.minecraft.client.gui.widget.EntryListWidget;
 import net.thbtt.favoritelanguage.client.FavoriteLanguageEntryAccess;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-
-@Mixin(EntryListWidget.class)
+@Mixin(ContainerWidget.class)
 public abstract class LanguageSelectionListWidgetMixin {
-    @Shadow
-    public abstract List<?> children();
-
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
     private void favoritelanguage$toggleFavoriteFromStarColumn(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
-        for (Object entry : this.children()) {
+        if (!((Object) this instanceof EntryListWidget<?> list)) {
+            return;
+        }
+        for (Object entry : list.children()) {
             if (entry instanceof FavoriteLanguageEntryAccess favoriteEntry && favoriteEntry.favoritelanguage$toggleFavoriteAt(mouseX, mouseY, button)) {
                 cir.setReturnValue(true);
                 return;
